b94c806546d2c060a44a330fe3fcbccef7ea6fda
Facundo Rocha <facundo.rocha@outlook.com>
2022-12-05 13:34:53 -0300

    TODO: complete CountingDuplicates

--- src/com/exercices/CountingDuplicates.java
+++ src/com/exercices/CountingDuplicates.java
@@ -3,50 +3,35 @@ package com.exercices;
 public class CountingDuplicates {
 
     public static void main(String[] args) {
-        System.out.println(countDuplicates("Hello world"));
+        System.out.println(countDuplicates("Helo world"));
     }
 
     public static int countDuplicates(String str) {
 
         str.replace(" ", "");
 
-        char[] charArray = str.toCharArray();
+        char[] charArray = str.toLowerCase().toCharArray();
 
-        int duplicatesCount = linearSearch(charArray);
+        int duplicatesCount = duplicatesLinearSearch(charArray);
 
         return duplicatesCount;
     }
 
-    public static char getPreviousChar(char[] chars) {
-        char initialChar = chars[0];
-        char previousChar = initialChar;
-        for (int i = 0; i < chars.length; i++) {
-
-            if(i != 0) {
-                return previousChar = chars[i - 1];
-            }
-
-        }
-        return previousChar;
-    }
-
-    public static int linearSearch(char[] chars) {
-        int count = 0;
+    public static int duplicatesLinearSearch(char[] chars) {
+        int duplicatesCount = 0;
         char initialChar = chars[0];
 
         for (int i = 0; i < chars.length; i++) {
             if(i >= 1) {
                 if(chars[i] == chars[i - 1]) {
-                    count++;
+                    duplicatesCount++;
                 }
             } else if(initialChar == chars[i + 1]) {
-                count++;
-            } else {
-                continue;
+                duplicatesCount++;
             }
 
         }
 
-        return count;
+        return duplicatesCount;
     }
 }
