737925dbd02394d4c486e843c942dce693aefc37
Facundo Rocha <facundo.rocha@outlook.com>
2022-12-05 12:33:00 -0300

    New exercise: StringConcat

---
+++ src/com/exercices/StringConcat.java
@@ -0,0 +1,18 @@
+package com.exercices;
+
+public class StringConcat {
+
+    public static void main(String[] args) {
+        String[] names = { "Facundo", "Daniela", "Nacho", "Richard" };
+
+        System.out.println(concat(names));
+    }
+
+    public static String concat(String[] arr) {
+        String namesConcat = "";
+        for (int i = 0; i < arr.length; i++) {
+            namesConcat += " " + arr[i];
+        }
+        return namesConcat.trim();
+    }
+}
